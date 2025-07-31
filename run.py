a0b2562c5a9d4c7644b28aac86d62d961033985b
smartjae <smartjea83@gmail.com>
2025-07-31 15:21:43 +0900

    Update run.py

--- run.py
+++ run.py
@@ -209,7 +209,9 @@ elif page == '감정 분석 AI의 작동원리':
         )
 
     # 오른쪽 컬럼: 버튼
+    
     with right_col:
+        st.markdown("### AI 모델 개발 도구")
         if st.button('➡️ Teachable Machine'):
             components.html(
                 """
